78313bc4e86c08d1c9e9b5ebac4b366f6004b19a
NilmerC <nilmer_12_2007@hotmail.com>
2026-08-16 01:52:08 -0500

    Add files via upload

--- SISTEMA PSE - CAT - GLOBAL.py
+++ SISTEMA PSE - CAT - GLOBAL.py
@@ -616,7 +616,8 @@ def ejecutar_sistema_pse():
 
   # Inyectar el historial completo y el partido actual en el HTML usando marcadores seguros
   historial_json_str = json.dumps(historial, ensure_ascii=False)
-  nuevo_bloque_js = f"let historialPartidos = {historial_json_str};\nlet currentMatchData = historialPartidos[0];"
+  nuevo_bloque_js = f"""let historialPartidos = {historial_json_str};
+let currentMatchData = (typeof currentMatchData !== 'undefined' && currentMatchData) ? currentMatchData : historialPartidos[0];"""
 
   try:
       with open("index.html", "r", encoding="utf-8") as file:
